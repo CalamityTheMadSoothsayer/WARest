35eda29dea0eeeb9be7764c43d545884483e20c7
CalamityTheMadSoothsayer <s3v3red.83@gmail.com>
2024-01-11 09:53:49 -0700

    sql fix?

--- Handlers/DataHandler/DataStorage.cs
+++ Handlers/DataHandler/DataStorage.cs
@@ -113,7 +113,7 @@ namespace WorldsAdriftServer.Handlers.DataHandler
                         connection.Open();
                         Console.WriteLine(connection.ConnectionString);
                         // Your SQL query to retrieve user data
-                        string sqlQuery = "SELECT * FROM \"userdata\"";
+                        string sqlQuery = "SELECT * FROM \"UserData\"";
 
                         using (NpgsqlCommand command = new NpgsqlCommand(sqlQuery, connection))
                         {
