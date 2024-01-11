354514a2953bdba28081953425e59c5953f8ab2b
CalamityTheMadSoothsayer <s3v3red.83@gmail.com>
2024-01-11 11:37:32 -0700

    rsahgre

--- Handlers/DataHandler/DataStorage.cs
+++ Handlers/DataHandler/DataStorage.cs
@@ -113,7 +113,7 @@ namespace WorldsAdriftServer.Handlers.DataHandler
                         connection.Open();
                         Console.WriteLine(connection.ConnectionString);
                         // Your SQL query to retrieve user data
-                        string sqlQuery = "SELECT * FROM userdata;";
+                        string sqlQuery = "SELECT * FROM pg_tables.userdata;";
 
 
                         using (NpgsqlCommand command = new NpgsqlCommand(sqlQuery, connection))
