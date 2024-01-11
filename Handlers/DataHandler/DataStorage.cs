6452eba254d12d29a0f65973688630ae9be1c66d
CalamityTheMadSoothsayer <s3v3red.83@gmail.com>
2024-01-11 10:25:53 -0700

    test

--- Handlers/DataHandler/DataStorage.cs
+++ Handlers/DataHandler/DataStorage.cs
@@ -113,7 +113,7 @@ namespace WorldsAdriftServer.Handlers.DataHandler
                         connection.Open();
                         Console.WriteLine(connection.ConnectionString);
                         // Your SQL query to retrieve user data
-                        string sqlQuery = "SELECT * FROM playerdata";
+                        string sqlQuery = "SELECT 1";
 
                         using (NpgsqlCommand command = new NpgsqlCommand(sqlQuery, connection))
                         {
