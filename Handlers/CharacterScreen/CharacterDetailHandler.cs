986d5759bbdc10c3c665839640a53dece6391a9f
CalamityTheMadSoothsayer <s3v3red.83@gmail.com>
2024-01-11 20:50:46 -0700

    tttest

--- Handlers/CharacterScreen/CharacterDetailHandler.cs
+++ Handlers/CharacterScreen/CharacterDetailHandler.cs
@@ -22,7 +22,26 @@ namespace WorldsAdriftServer.Handlers.CharacterScreen
                 var seenIntro = requestBody["Misc"]["Seen Intro"]?.ToString();
                 var skippedTutorial = requestBody["Misc"]["Skipped Tutorial"]?.ToString();
 
-                // TODO: Extract other properties as needed
+                // Extract Head properties
+                var headId = requestBody["Head"]["Id"]?.ToString();
+                var headPrefab = requestBody["Head"]["Prefab"]?.ToString();
+
+                // Extract Body properties
+                var bodyId = requestBody["Body"]["Id"]?.ToString();
+                var bodyPrefab = requestBody["Body"]["Prefab"]?.ToString();
+
+                // Extract Feet properties
+                var feetId = requestBody["Feet"]["Id"]?.ToString();
+                var feetPrefab = requestBody["Feet"]["Prefab"]?.ToString();
+
+                // Extract Face properties
+                var faceId = requestBody["Face"]["Id"]?.ToString();
+                var facePrefab = requestBody["Face"]["Prefab"]?.ToString();
+
+                // Extract Facial Hair properties
+                var facialHairId = requestBody["Facial Hair"]["Id"]?.ToString();
+                var facialHairPrefab = requestBody["Facial Hair"]["Prefab"]?.ToString();
+
 
                 using (NpgsqlConnection connection = new NpgsqlConnection(DataHandler.DataStorage.connectionString))
                 {
@@ -30,8 +49,10 @@ namespace WorldsAdriftServer.Handlers.CharacterScreen
 
                     // Insert character details into the database
                     string insertCharacterSql = $@"
-                        INSERT INTO CharacterDetails (userKey, characterUid, name, server, serverIdentifier, gender, seenIntro, skippedTutorial)
-                        VALUES ('{userKey}', '{characterUid}', '{name}', '{server}', '{serverIdentifier}', '{gender}', '{seenIntro}', '{skippedTutorial}')
+                        INSERT INTO CharacterDetails (userKey, characterUid, name, server, serverIdentifier, gender, seenIntro, skippedTutorial,
+                        headId, headPrefab, bodyId, bodyPrefab, feetId, feetPrefab, faceId, facePrefab, facialHairId, facialHairPrefab)
+                        VALUES ('{userKey}', '{characterUid}', '{name}', '{server}', '{serverIdentifier}', '{gender}', '{seenIntro}', '{skippedTutorial}',
+                        '{headId}', '{headPrefab}', '{bodyId}', '{bodyPrefab}', '{feetId}', '{feetPrefab}', '{faceId}', '{facePrefab}', '{facialHairId}', '{facialHairPrefab}')
                     ";
 
                     using (NpgsqlCommand insertCharacterCommand = new NpgsqlCommand(insertCharacterSql, connection))
@@ -57,3 +78,36 @@ namespace WorldsAdriftServer.Handlers.CharacterScreen
         }
     }
 }
+
+//Properties:
+//Id: 0
+
+//    Character UID:0094183f - 23c6 - 4775 - 9df1 - 60eddda37d1f
+//    Name:warsome
+//    Server:serverName?
+//    Server Identifier:community_server
+
+//Head:
+//	Id: h
+//    Prefab:hair_mohawk
+
+//Body:
+//	Id: t
+//    Prefab:torso_ponchoVariantB
+
+//Feet:
+//	Id: l
+//    Prefab:legs_boots
+
+//Face:
+//	Id: f
+//    Prefab:face_B
+
+//Facial Hair:
+//	Id: f
+//    Prefab:facialHair_hunterBeard
+
+//Misc:
+//	Gender: Male
+//    Seen Intro: Yes
+//    Skipped Tutorial: Yes
