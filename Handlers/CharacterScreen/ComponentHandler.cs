fb3e48a42e292d46980921d8246e0eb94fe6ff11
CalamityTheMadSoothsayer <s3v3red.83@gmail.com>
2024-01-14 20:57:46 -0700

    get component change

--- Handlers/CharacterScreen/ComponentHandler.cs
+++ Handlers/CharacterScreen/ComponentHandler.cs
@@ -1,6 +1,5 @@
 ﻿using Npgsql;
 using Newtonsoft.Json.Linq;
-using System;
 using NetCoreServer;
 using WorldsAdriftServer.Utilities;
 
@@ -10,42 +9,48 @@ namespace WorldsAdriftServer.Handlers.CharacterScreen
     {
         private static string connectionString = DataHandler.DataStorage.connectionString;
 
-       internal static void getComponentDate(HttpSession session, HttpRequest request)
+        internal static void getComponentDate(HttpSession session, HttpRequest request)
         {
             JObject requestBody = JObject.Parse(request.Body);
 
             try
             {
                 long entityId = Convert.ToInt64(requestBody["entityId"]);
-                long componentId = Convert.ToInt64(requestBody["componentId"]);
 
                 using (var connection = new NpgsqlConnection(connectionString))
                 {
                     connection.Open();
 
-                    using (var cmd = new NpgsqlCommand("SELECT data FROM gameobjects WHERE entityid = @entityId AND componentid = @componentId", connection))
+                    using (var cmd = new NpgsqlCommand("SELECT componentid, data FROM gameobjects WHERE entityid = @entityId", connection))
                     {
                         cmd.Parameters.AddWithValue("entityId", entityId);
-                        cmd.Parameters.AddWithValue("componentId", componentId);
 
                         using (var reader = cmd.ExecuteReader())
                         {
-                            if (reader.Read())
-                            {
-                                string jsonData = reader.GetString(0);
+                            JObject responseData = new JObject();
 
-                                // Build response
+                            while (reader.Read())
+                            {
+                                long componentId = reader.GetInt64(0);
+                                string jsonData = reader.GetString(1);
+
+                                // Add component data to the JSON response
+                                responseData.Add(new JProperty($"component_{componentId}", JObject.Parse(jsonData)));
+                            }
+
+                            if (responseData.Count > 0)
+                            {
+                                // Build response with entityId as the root and components as keys
                                 Utilities.ResponseBuilder.BuildAndSendResponse(
                                     session,
                                     (int)RequestRouterHandler.status,
                                     "entityId", entityId,
-                                    "componentId", componentId,
-                                    "data", jsonData
+                                    "components", responseData
                                 );
                             }
                             else
                             {
-                                ResponseBuilder.BuildAndSendResponse(session, 500, "status", "No data found for the specified entity and component.");
+                                ResponseBuilder.BuildAndSendResponse(session, 500, "status", "No data found for the specified entity.");
                             }
                         }
                     }
@@ -56,6 +61,7 @@ namespace WorldsAdriftServer.Handlers.CharacterScreen
                 ResponseBuilder.BuildAndSendResponse(session, 500, "status", "error: " + ex.Message);
             }
         }
+
 
         internal static void setComponentDate(HttpSession session, HttpRequest request)
         {
